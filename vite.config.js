f32f229097364ee48af6546deb98171fe8a51baf
Marcel Marques <marcmatiasdev@gmail.com>
2023-07-05 10:52:58 -0300

    Updating project name

--- vite.config.js
+++ vite.config.js
@@ -11,18 +11,18 @@ export default defineConfig({
     minify: true,
     lib: {
       entry: path.resolve(__dirname, "src/main.js"),
-      name: "Dashboard",
-      fileName: () => `dashboard.js`,
+      name: "MCT",
+      fileName: () => `mct.js`,
 
       formats: ['umd']
     },
     rollupOptions: {
       output: {
-        assetFileNames: "dashboard.[ext]",
+        assetFileNames: "mct.[ext]",
       },
     },
   },
-  define: { 
+  define: {
     'process.env.NODE_ENV': '"production"',
     '__VUE_OPTIONS_API__': true,
     '__VUE_PROD_DEVTOOLS__': true,
